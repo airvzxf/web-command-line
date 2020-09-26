7c881be642e0b6317153bc18fcc109446367fdd0
Israel Roldan <israel.alberto.rv@gmail.com>
2020-09-26 05:07:21 -0500

    Redirected the error outputs to the standard output also added a TODO.
    Signed-off-by: Israel Roldan <israel.alberto.rv@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Web: Command line
 Run bash command in a web page.
+
+TODOs:
+- Encode the input text and decode to allow special characters as + and &.
--- public_html/shell.sh
+++ public_html/shell.sh
@@ -2,7 +2,8 @@
 echo "Content-type: text/plain"
 echo ""
 set -exv
-echo "${1}"
+echo "1: ${1}"
+echo "QUERY_STRING: $QUERY_STRING"
 echo ""
 # Comment
-eval "${1}"
+eval "${1}" 2>&1
