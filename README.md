c7ff18bde01b8acb562e3a82a73f522a4984dfb3
Israel Roldan <israel.alberto.rv@gmail.com>
2020-09-26 05:45:24 -0500

    Fixed the URL decode function.
    Signed-off-by: Israel Roldan <israel.alberto.rv@gmail.com>

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # Web: Command line
 Run bash command in a web page.
-
-TODOs:
-- Encode the input text and decode to allow special characters as + and &.
--- public_html/shell.sh
+++ public_html/shell.sh
@@ -2,15 +2,12 @@
 echo "Content-type: text/plain"
 echo ""
 function urlDecode() {
-  : "${*//+/ }"
-  echo -e "${_//%/\\x}"
+  echo -e "${*//%/\\x}"
 }
 set -xv
 COMMAND=$(urlDecode "${QUERY_STRING}")
 echo "Argument #1:  ${1}"
 echo "QUERY_STRING: ${QUERY_STRING}"
 echo "COMMAND:      ${COMMAND}"
-echo ""
-eval "${1}" 2>&1
 echo ""
 eval "${COMMAND}" 2>&1
