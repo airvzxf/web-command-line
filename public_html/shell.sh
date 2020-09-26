db153ab1f65238e31a6057b5ee257d7b3e65b9fd
Israel Roldan <israel.alberto.rv@gmail.com>
2020-09-26 02:35:47 -0500

    Added header to the shell script to return as a text/plain response.
    Signed-off-by: Israel Roldan <israel.alberto.rv@gmail.com>

--- public_html/shell.sh
+++ public_html/shell.sh
@@ -1,14 +1,17 @@
-#!/bin/bash -evx
-
-FILE_NAME='./shell.txt'
+#!/bin/bash -e
+echo "Content-type: text/plain"
+set -vx
 
 function print_command() {
-  eval "${1}" &>>${FILE_NAME}
-  echo -e "\n" &>>${FILE_NAME}
+  eval "${1}"
+  echo -e "\n"
 }
 
-echo "" &>${FILE_NAME}
-
+echo ""
+echo "0: ${0}"
+echo "1: ${1}"
+echo "?: ${?}"
+echo ""
 print_command 'ls -lhai /usr/bin/'
 
 # sadkjf laksjf lkasf laksf jlskdf lasjf lsdf lksjflskjf lksajdf laskdjf lskdfj laskdjf lasdjf lksadjfls
